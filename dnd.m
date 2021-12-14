57b1902334371564c1e2aa9fa5e7c50d7c76be6e
kushagra1912 <agrawalkushagra647@gmail.com>
2021-12-15 00:03:38 +0530

    DnD Fixed

--- dnd.m
+++ dnd.m
@@ -4,13 +4,18 @@ g= @(x) exp(x);
 p = 2.25;
 n = 4;
 
-for 1 = 1:n
-	for j = 1:n
-		F(i,j) = (F(i-1,j)-F(i-1,j-1))/x(j)-x(j-i+1);
+
+for i = 1:n
+	F(1,i)=f(i);
+end
+
+for i = 2:n
+	for j = i:n
+		F(i,j) = (F(i-1,j)-F(i-1,j-1))/(x(j)-x(j-i+1));
 	end
 end
 for i = 2:n
-	prod(1) = 1;
+	prod(i) = 1;
 	for j = 1:i-1
 		prod(i)=prod(i)*(p-x(j));
 	end
