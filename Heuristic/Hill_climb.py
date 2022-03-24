b0e29a87e626f2c23271c9347382aa25878eeec5
Kushagra Agrawal <agrawalkushagra647@gmail.com>
2022-03-24 23:46:57 +0530

    updated hill climb

--- Heuristic/Hill_climb.py
+++ Heuristic/Hill_climb.py
@@ -1,122 +1,147 @@
-import numpy as np
-from copy import deepcopy as dc
+from copy import deepcopy as deepc
 
 q = []
 visited = []
 
-def dist(s,g):
-    d = 0
-    for i in range(len(s)):
-        for j in range(len(s)):
-            if s[i][j] != g[i][j]:
-                d += 1
-    return d
-
-def line():
-    print('-'*50)
-
-def enqueue(s):
-    flag = 0
-    for i in visited:
-        if compare(i, s):
-            flag = 1
-            break
-    if not flag:
-        q.append(s)
-
-def dequeue(): 
-    q.sort()
-    a = q.pop(0)
-    b = a[1]
-    q.clear()
-    return b
-
-def compare(s, g):
-    if np.all(s==g) :
+def compare(s0, g):
+    if (s0 == g):
         return 1
     else:
         return 0
 
 def findPos(s):
-    for i in range(len(s)):
-        for j in range(len(s[1])):
-            if s[i][j] == -1:
+    for i in range (len(s)):
+        for j in range (len(s[0])):
+            if (s[i][j] == 0):
                 return [i,j]
 
 def up(s, pos):
-    s1 = dc(s)
-    if pos[0] == 0:
+    row = pos[0]
+    col = pos[1]
+    s1 = deepc(s)
+    if row == 0:
         return s1
     else:
-        s1[pos[0]-1][pos[1]], s1[pos[0]][pos[1]] = s1[pos[0]][pos[1]], s1[pos[0]-1][pos[1]]             
+        s1[row][col], s1[row-1][col] = s1[row-1][col], s1[row][col]
+        print("---------------------------------------------------------")
+        print(s1)
         return s1
 
 def down(s, pos):
-    s1 = dc(s)
-    if pos[0] == len(s1)-1:
-        
+    row = pos[0]
+    col = pos[1]
+    s1 = deepc(s)
+    if row == (len(s1) - 1):
         return s1
     else:
-        s1[pos[0]+1][pos[1]], s1[pos[0]][pos[1]] = s1[pos[0]][pos[1]], s1[pos[0]+1][pos[1]]        
+        s1[row][col], s1[row+1][col] = s1[row+1][col], s1[row][col]
+        print("---------------------------------------------------------")
+        print(s1)
         return s1
 
 def left(s, pos):
-    s1 = dc(s)
-    if pos[1] == 0:
+    row = pos[0]
+    col = pos[1]
+    s1 = deepc(s)
+    if col == 0:
         return s1
     else:
-        s1[pos[0]][pos[1]-1], s1[pos[0]][pos[1]] = s1[pos[0]][pos[1]], s1[pos[0]][pos[1]-1]        
+        s1[row][col], s1[row][col-1] = s1[row][col-1], s1[row][col]
+        print("---------------------------------------------------------")
+        print(s1)
         return s1
 
 def right(s, pos):
-    s1 = dc(s)
-    if pos[1] == 2:
+    row = pos[0]
+    col = pos[1]
+    s1 = deepc(s)
+    if col == (len(s1[row]) - 1):
         return s1
     else:
-        s1[pos[0]][pos[1]+1], s1[pos[0]][pos[1]] = s1[pos[0]][pos[1]], s1[pos[0]][pos[1]+1]        
+        s1[row][col], s1[row][col+1] = s1[row][col+1], s1[row][col]
+        print("---------------------------------------------------------")
+        print(s1)
         return s1
 
-def main():
-    g = [[1,2,3],[8,-1,4],[7,6,5]]
-    s0 = [[2,-1,3],[1,8,4],[7,6,5]]
-    curr_state = dc(s0)
-    print(curr_state)
-    while 1:
-        pos = findPos(curr_state)
-        s1 = up(curr_state, pos)
-        d = dist(s1,g)
-        if not compare(s1, g):
-            line()
-            print(s1)
-            enqueue([d,s1])
-        else: break
-        s1 = down(curr_state, pos)
-        d = dist(s1,g)
-        if not compare(s1, g):
-            line()
-            print(s1)
-            enqueue([d,s1])
-        else: break        
-        s1 = left(curr_state, pos)
-        d = dist(s1,g)
-        if not compare(s1, g):
-            line()
-            print(s1)
-            enqueue([d,s1])
-        else: break        
-        s1 = right(curr_state, pos)
-        d = dist(s1,g)
-        if not compare(s1, g):
-            line()
-            print(s1)
-            enqueue([d,s1])
-        else: break
-        d = dist(curr_state,g)
-        visited.append([d,curr_state])
-        new = dequeue()
-        if(dist(new,g) >= dist(curr_state,g)):
-            print("The best solution is {}".format(curr_state))
+def dist(cs, gs):
+    d = 0
+    for i in range (len(cs)):
+        for j in range (len(cs[i])):
+            if (cs[i][j] != gs[i][j]):
+                d += 1
+    return d
+
+def enqueue(ds):
+    flag = 0
+    for i in visited:
+        if compare(i, ds[1]):
+            flag = 1
             break
+    if flag == 0:
+        q.append(ds)
+
+def dequeue():
+    q.sort()
+    if (len(q) == 0):
+        print("Goal can't be reached")
+        exit()
+    else:
+        a = q.pop(0)
+        elem = a[1]
+        q.clear()
+        return elem
+
+def main():
+    s0 = [[2,0,3], [1,8,4], [7,6,5]]
+    goal = [[1,2,3], [8,0,4], [7,6,5]]
+
+    curr_state = deepc(s0)
+    g = deepc(goal)
+    
+    while (1):
+        pos = findPos(curr_state)
+        # print (pos)
         
+        new_state = up(curr_state, pos)
+        dis = dist(curr_state, g)
+        if compare(new_state, g) == 1:
+            print("Found")
+            exit()
+        else:
+            enqueue([dis, new_state])
+        
+        new_state = down(curr_state, pos)
+        dis = dist(curr_state, g)
+        if compare(new_state, g) == 1:
+            print("Found")
+            exit()
+        else:
+            enqueue([dis, new_state])
+        
+        new_state = right(curr_state, pos)
+        dis = dist(curr_state, g)
+        if compare(new_state, g) == 1:
+            print("Found")
+            exit()
+        else:
+            enqueue([dis, new_state])
+        
+        new_state = left(curr_state, pos)
+        dis = dist(curr_state, g)
+        if compare(new_state, g) == 1:
+            print("Found")
+            exit()
+        else:
+            enqueue([dis, new_state])
+
+        visited.append(curr_state)
+        new = dequeue()
+        if dist(new, g) >= dist(curr_state, g):
+            print("The Best Solution Found is: ", curr_state)
+            exit()
+        print("---------------------------------------------------------")
+    
+    
+
 if __name__ == '__main__':
     main()
